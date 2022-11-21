866f9de232887d7ab772f64de64f68d1f42a1d3e
johan2097 <102374506+johan2097@users.noreply.github.com>
2022-11-21 12:26:03 -0500

    Add files via upload

---
+++ src/components/FilterCountry/FilterCountry.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const FilterCountry = ({ onSelect }) => {
+  const selectHandler = (e) => {
+    const regionName = e.target.value;
+    onSelect(regionName);
+  };
+
+  return (
+    <select onChange={selectHandler}>
+      <option className="option">Opciones</option>
+      <option className="option" value="Africa">
+        Africa
+      </option>
+      <option className="option" value="America">
+        America
+      </option>
+      <option className="option" value="Asia">
+        Asia
+      </option>
+      <option className="option" value="Europe">
+        Europa
+      </option>
+      <option className="option" value="Oceania">
+        Oceania
+      </option>
+    </select>
+  );
+};
+
+export default FilterCountry;
